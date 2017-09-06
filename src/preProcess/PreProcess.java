901716f95efe0420bd2bdd6be3487337a7c2fc9f
iamlockelightning <iamlockelightning@gmail.com>
2017-09-06 19:05:20 +0800

    Generate en_zh_cl_id_title.txt

--- src/preProcess/PreProcess.java
+++ src/preProcess/PreProcess.java
@@ -17,10 +17,12 @@ public class PreProcess {
 		PreProcess pp = new PreProcess();
 		Date start_date = new Date();
 		System.out.println("Extraction starts at:" + start_date);
-		String location = "/home/lcj/WikiExtractor/etc/";
+		
+//		String location = "/home/lcj/WikiExtractor/etc/";
+		String location = "/Users/locke/Desktop/preprocess/";
 		
 		pp.filter(location+"zhwiki-latest-langlinks.2en.result.sql", location+"enwiki-latest-pages-articles-multistream.id_titleresult.id_titlexml", location+"zhwiki-latest-pages-articles-multistream.id_titleresult.id_titlexml");
-//		ze.filter("/home/lcj/zhwiki-latest-langlinks.sql");
+		
 		Date end_date = new Date();
 		double cost = (double)(end_date.getTime()-start_date.getTime())/1000.0/60.0;
 		System.out.println("Extraction ents at: " + end_date + "\tcost: " + cost + "min");
@@ -47,12 +49,12 @@ public class PreProcess {
             	}
             }
             if (contains == false) {
-            	bufferedWriter.write(line);
+            	bufferedWriter.write(line + "\n");
             }
         }
         bufferedReader.close();
         bufferedWriter.close();
-        
+		
         Map<String, String> cl_zh_id2title_dict = read_w_t_t_w(zh_cl.replace("result.", "result.new."), 0);
         Map<String, String> cl_zh_title2id_dict = read_w_t_t_w(zh_cl.replace("result.", "result.new."), 1);
 
@@ -62,25 +64,25 @@ public class PreProcess {
 		Map<String, String> zh_id2title_dict = read_w_t_t_w(zh_id_title, 0);
 		Map<String, String> zh_title2id_dict = read_w_t_t_w(zh_id_title, 1);
 		
+		
 		Set<String> common_zh_id = new HashSet<String>();
 		common_zh_id.addAll(zh_id2title_dict.keySet());
 		common_zh_id.retainAll(cl_zh_id2title_dict.keySet());
-		
-		Set<String> common_en_title = new HashSet<String>();
-		common_en_title.addAll(cl_zh_title2id_dict.keySet());
-		common_en_title.retainAll(en_title2id_dict.keySet());
-		
-		System.out.println("lalalal");
         System.out.println(common_zh_id.size());
+        
+        
+		Set<String> common_en_title = new HashSet<String>();
+		common_en_title.retainAll(en_title2id_dict.keySet());
+		common_en_title.addAll(cl_zh_title2id_dict.keySet());
         System.out.println(common_en_title.size());
         
-//        bufferedWriter = new BufferedWriter(new FileWriter(new File("en_zh_cl_id_title.txt")));
-//        for (String k : cl_zh_id2title_dict.keySet()) {
-//        	if (common_zh_id.contains(k) && common_en_title.contains(cl_zh_id2title_dict.get(k))) {
-//        		bufferedWriter.write(cl_zh_id2title_dict.get(k) + "\t\t" + en_title2id_dict.get(cl_zh_id2title_dict.get(k)) + "\t\t" + zh_id2title_dict.get(k) + "\t\t" + k + "\n");
-//        	}
-//        }
-//        bufferedWriter.close();
+        bufferedWriter = new BufferedWriter(new FileWriter(new File("en_zh_cl_id_title.txt")));
+        for (String k : cl_zh_id2title_dict.keySet()) {
+        	if (common_zh_id.contains(k) && common_en_title.contains(cl_zh_id2title_dict.get(k))) {
+        		bufferedWriter.write(cl_zh_id2title_dict.get(k) + "\t\t" + en_title2id_dict.get(cl_zh_id2title_dict.get(k)) + "\t\t" + zh_id2title_dict.get(k) + "\t\t" + k + "\n");
+        	}
+        }
+        bufferedWriter.close();
 	}
 	
 	public Map<String, String> read_w_t_t_w(String filename, int key_pos) throws Exception {
