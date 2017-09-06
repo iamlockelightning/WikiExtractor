d2bebdb6fbd746263800fd92277dae535e16b674
iamlockelightning <iamlockelightning@gmail.com>
2017-09-06 19:32:56 +0800

    Added getCLEntities().

--- src/preProcess/PreProcess.java
+++ src/preProcess/PreProcess.java
@@ -11,6 +11,9 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.collections4.Put;
+import org.json.JSONObject;
+
 public class PreProcess {
 
 	public static void main(String args[]) throws Exception {
@@ -18,17 +21,72 @@ public class PreProcess {
 		Date start_date = new Date();
 		System.out.println("Extraction starts at:" + start_date);
 		
-//		String location = "/home/lcj/WikiExtractor/etc/";
-		String location = "/Users/locke/Desktop/preprocess/";
+		String location = "/home/lcj/WikiExtractor/etc/";
+//		String location = "/Users/locke/Desktop/preprocess/";
 		
-		pp.filter(location+"zhwiki-latest-langlinks.2en.result.sql", location+"enwiki-latest-pages-articles-multistream.id_titleresult.id_titlexml", location+"zhwiki-latest-pages-articles-multistream.id_titleresult.id_titlexml");
+//		pp.CLFilter(location+"zhwiki-latest-langlinks.2en.result.sql", location+"enwiki-latest-pages-articles-multistream.id_titleresult.id_titlexml", location+"zhwiki-latest-pages-articles-multistream.id_titleresult.id_titlexml");
+		pp.getCLEntities(location+"en_zh_cl_id_title.txt", location+"enwiki-latest-pages-articles-multistream.result.xml", location+"zhwiki-latest-pages-articles-multistream.result.xml");
 		
 		Date end_date = new Date();
 		double cost = (double)(end_date.getTime()-start_date.getTime())/1000.0/60.0;
 		System.out.println("Extraction ents at: " + end_date + "\tcost: " + cost + "min");
 	}
 	
-	public void filter(String zh_cl, String en_id_title, String zh_id_title) throws Exception {
+	public void getCLEntities(String en_zh_cl_id_title, String en_pages, String zh_pages) throws Exception {
+		
+		Map<String, String> cl_en_id2title = new HashMap<String, String>();
+		Map<String, String> cl_en_title2id = new HashMap<String, String>();
+		Map<String, String> cl_zh_id2title = new HashMap<String, String>();
+		Map<String, String> cl_zh_title2id = new HashMap<String, String>();		
+		BufferedReader bufferedReaderCL = new BufferedReader(new FileReader(new File(en_zh_cl_id_title)));
+		String line = new String();
+        while (true) {
+            line = bufferedReaderCL.readLine();
+            if (line == null) {
+            	break;
+            }
+            String[] words = line.split("\t\t");
+            cl_en_id2title.put(words[0], words[1]);
+            cl_en_title2id.put(words[1], words[0]);
+            cl_zh_id2title.put(words[2], words[3]);
+            cl_zh_title2id.put(words[3], words[2]);
+        }
+        bufferedReaderCL.close();
+		
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(en_pages)));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(en_pages.replace(".xml", ".cl.xml"))));
+        line = new String();
+        while (true) {
+            line = bufferedReader.readLine();
+            if (line == null) {
+            	break;
+            }
+            JSONObject page = new JSONObject(line);
+            if (cl_en_id2title.containsKey( page.get("id") ) && cl_en_title2id.containsKey( page.get("title") )) {
+            	bufferedWriter.write(line + "\n");
+            }
+        }
+        bufferedReader.close();
+        bufferedWriter.close();
+        
+        bufferedReader = new BufferedReader(new FileReader(new File(zh_pages)));
+        bufferedWriter = new BufferedWriter(new FileWriter(new File(zh_pages.replace(".xml", ".cl.xml"))));
+        line = new String();
+        while (true) {
+            line = bufferedReader.readLine();
+            if (line == null) {
+            	break;
+            }
+            JSONObject page = new JSONObject(line);
+            if (cl_zh_id2title.containsKey( page.get("id") ) && cl_zh_title2id.containsKey( page.get("title") )) {
+            	bufferedWriter.write(line + "\n");
+            }
+        }
+        bufferedReader.close();
+        bufferedWriter.close();
+	}
+	
+	public void CLFilter(String zh_cl, String en_id_title, String zh_id_title) throws Exception {
     	System.out.println("Processing file: " + zh_cl);
         BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(zh_cl)));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(zh_cl.replace("result.", "result.new."))));
